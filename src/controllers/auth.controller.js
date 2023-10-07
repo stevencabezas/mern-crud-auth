b3297ae1c669e0845ca629b349ccf525803748d8
stevencabezas <cabezasstevenr@gmail.com>
2023-10-07 09:17:21 -0600

    cookie updated v2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,7 +49,8 @@ export const login = async (req, res) => {
         const token = await createAccessToken({id: userFound._id});
         
         res.cookie('token', token, {
-            sameSite: "none"
+            sameSite: "none",
+            secure: "false",
         });
         res.json({
             id: userFound._id,
