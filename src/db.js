ad4348b89e0d0bea78d9f85d13a2b78363139b43
stevencabezas <cabezasstevenr@gmail.com>
2023-10-07 08:27:54 -0600

    db updated

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect("mongodb://127.0.0.1/merndb");
+        await mongoose.connect("mongodb+srv://kitososx3:9Yj4QqMNYkKI3hIA@mern-restful.arzkuum.mongodb.net/?retryWrites=true&w=majority");
         console.log(">>> DB is connected");
     }catch(error){
         console.log("Here the error: "+error); 
